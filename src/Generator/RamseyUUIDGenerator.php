bb057d2d3c1816f6d7bbaad163bdf6e74e3f891b
frank <frank@qlimix.net>
2019-02-02 13:58:08 +0100

    pass an uuid instead of ramsey uuid behind an interface

--- src/Generator/RamseyUUIDGenerator.php
+++ src/Generator/RamseyUUIDGenerator.php
@@ -2,13 +2,30 @@
 
 namespace Qlimix\Id\UUID\Generator;
 
-use Qlimix\Id\UUID\RamseyUUID;
+use Qlimix\Id\UUID\Generator\Exception\UUIDGeneratorException;
 use Qlimix\Id\UUID\UUID;
+use Ramsey\Uuid\UuidFactory;
+use Throwable;
 
 final class RamseyUUIDGenerator implements UUIDGeneratorInterface
 {
+    /** @var UuidFactory */
+    private $factory;
+
+    public function __construct(UuidFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function generate(): UUID
     {
-        return new RamseyUUID(\Ramsey\Uuid\Uuid::uuid4());
+        try {
+            return new UUID($this->factory->uuid4()->toString());
+        } catch (Throwable $exception) {
+            throw new UUIDGeneratorException('Failed to generate an UUID', 0, $exception);
+        }
     }
 }
